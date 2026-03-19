629797608a791fa6b8f58da929de9cd5058dc1b5
Moritz <74018068+MoritzLue24@users.noreply.github.com>
2026-03-19 22:18:00 +0100

    Fix formatting of Email property in UserCreateDto

--- Api/DTOs/Users/UserCreateDto.cs
+++ Api/DTOs/Users/UserCreateDto.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 
+
 namespace Api.DTOs.Users
 {
     public class UserCreateDto
@@ -7,7 +8,7 @@ namespace Api.DTOs.Users
         [Required(ErrorMessage = "Email is required")]
         [EmailAddress(ErrorMessage = "Invalid email format")]
         [StringLength(255, ErrorMessage = "Email must not exceed 255 characters")]
-        public required string  Email { get; set; } 
+        public required string Email { get; set; } 
         
         [Required(ErrorMessage = "Password is required")]
         [MinLength(8, ErrorMessage = "Password must be at least 8 characters")]
