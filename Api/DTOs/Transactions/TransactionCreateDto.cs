125748850cb39d246b0d9de97ef0b99b243af250
Moritz <74018068+MoritzLue24@users.noreply.github.com>
2026-03-19 22:20:43 +0100

    Modify CounterParty property in TransactionCreateDto
    Updated CounterParty property to be optional and initialized to an empty string.

--- Api/DTOs/Transactions/TransactionCreateDto.cs
+++ Api/DTOs/Transactions/TransactionCreateDto.cs
@@ -13,9 +13,8 @@ namespace Api.DTOs.Transactions
         ErrorMessage = "Max. 12 Stellen vor und 2 nach dem Komma")] // Allows up to 12 digits before the decimal point and 2 digits after
         public decimal Amount { get; set; } 
         
-        [Required(ErrorMessage = "CounterParty is required")]
         [StringLength(255, ErrorMessage = "CounterParty must not exceed 255 characters")]
-        public required string CounterParty { get; set; } 
+        public string CounterParty { get; set; } = string.Empty; 
         
         [Required(ErrorMessage = "Title is required")]
         [StringLength(500, ErrorMessage = "Title must not exceed 500 characters")]
