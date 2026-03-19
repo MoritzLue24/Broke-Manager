258a7633e2ee751d48ec5d08fcb8bcaf0fefa079
Moritz <74018068+MoritzLue24@users.noreply.github.com>
2026-03-19 22:23:27 +0100

    Update TransactionResponseDto properties
    Changed CounterParty property to be optional and initialized to an empty string.

--- Api/DTOs/Transactions/TransactionsResponseDto.cs
+++ Api/DTOs/Transactions/TransactionsResponseDto.cs
@@ -3,12 +3,11 @@ namespace Api.DTOs.Transactions
     public class TransactionResponseDto
     {
         public int Id { get; set; }
-        public DateOnly Date { get; set; } //Datetime??
+        public DateOnly Date { get; set; }
         public decimal Amount { get; set; }
-        public required string CounterParty { get; set; }
+        public string CounterParty { get; set; } = string.Empty;
         public required string Title { get; set; }
         public int CategoryId { get; set; }
         public required string CategoryName { get; set; }
-
     }
 }
