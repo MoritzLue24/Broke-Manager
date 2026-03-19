0bd19a11541148a7ab952d665df1c926bbda3374
Moritz <74018068+MoritzLue24@users.noreply.github.com>
2026-03-19 22:29:11 +0100

    Refactor CategoryUpdateDto by removing Keywords initialization
    Removed Keywords property initialization and updated using directives.

--- Api/DTOs/Categories/CategoryUpdateDto.cs
+++ Api/DTOs/Categories/CategoryUpdateDto.cs
@@ -1,6 +1,7 @@
 using System.ComponentModel.DataAnnotations;
-using Api.DTOs.Intervals;
+using Api.DTOs;
 using Api.DTOs.Keywords;
+
 
 namespace Api.DTOs.Categories
 {
@@ -10,8 +11,6 @@ namespace Api.DTOs.Categories
         [StringLength(255, ErrorMessage = "Name must not exceed 255 characters")]
         public required string Name { get; set; }
 
-        public List<KeywordUpdateDto> Keywords { get; set; } = []; // = new List<KeywordCreateDto>();
-        
         public IntervalDto Interval { get; set; } = IntervalDto.Once; 
     }
 }
