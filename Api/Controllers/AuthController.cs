7eab9d927d4fe518a07c14bf4b0d9528b130f99a
MoritzLue24 <moluecke@gmx.de>
2026-04-06 23:55:10 +0200

    Used AuthService in AuthController

--- Api/Controllers/AuthController.cs
+++ Api/Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Api.DTOs.Auth;
+using Api.Services.Auth;
 
 
 namespace Api.Controllers
@@ -8,23 +9,27 @@ namespace Api.Controllers
     [Route("api/auth")]
     public class AuthController : ControllerBase
     {
-        public AuthController()
-        {
+        private readonly IAuthService _authService;
 
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
         }
 
         [HttpPost("register")]
         public async Task<ActionResult> RegisterUser([FromBody] RegisterRequestDto registerDto)
         {
-            await Task.CompletedTask;
-            return NoContent();
+            var userClaims = await _authService.RegisterUserAsync(registerDto);
+            return Ok(new { Token = _authService.GenJwtToken(userClaims) });
         }
 
         [HttpPost("login")]
         public async Task<ActionResult> LoginUser([FromBody] LoginRequestDto loginDto)
         {
-            await Task.CompletedTask;
-            return NoContent();
+            var userClaims = await _authService.ValidateUserAsync(loginDto);
+            if (userClaims == null)
+                return Unauthorized(new { Message = "Email or Password is incorrect" });
+            return Ok(new { Token = _authService.GenJwtToken(userClaims) });
         }
     }
 }
