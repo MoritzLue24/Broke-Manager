359f2172c2041ae6754464bd6f0033cc11dfbc8a
MoritzLue24 <moluecke@gmx.de>
2026-04-06 23:54:56 +0200

    Used DTOs correctly, GenJwtToken implemented

--- Api/Services/Auth/AuthService.cs
+++ Api/Services/Auth/AuthService.cs
@@ -1,8 +1,12 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Api.Data;
 using Api.DTOs.Auth;
 using Api.Exceptions;
 using Api.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 
 namespace Api.Services.Auth
@@ -10,48 +14,80 @@ namespace Api.Services.Auth
     public class AuthService : IAuthService
     {
         private readonly AppDbContext _dbContext;
+        private readonly IConfiguration _config;
 
-        public AuthService(AppDbContext dbContext)
+        public AuthService(AppDbContext dbContext, IConfiguration config)
         {
             _dbContext = dbContext;
+            _config = config;
         }
 
-        public async Task<AuthResponseDto> RegisterUserAsync(string email, string password)
+        public async Task<UserClaimsDto> RegisterUserAsync(RegisterRequestDto registerDto)
         {
-            if (await _dbContext.Users.AnyAsync(u => u.Email == email))
-                throw new AlreadyExistsException($"User with email '{email}' already exists");
+            if (await _dbContext.Users.AnyAsync(u => u.Email == registerDto.Email))
+                throw new AlreadyExistsException($"User with email '{registerDto.Email}' already exists");
 
             User newUser = new User
             {
-                Email = email,
-                Password = password // TODO: Hashish
+                Email = registerDto.Email,
+                Password = registerDto.Password // TODO: Hashish
             };
             _dbContext.Users.Add(newUser);
             await _dbContext.SaveChangesAsync();
 
-            return new AuthResponseDto
+            return new UserClaimsDto
             {
                 UserId = newUser.Id
             };
         }
 
-        public async Task<AuthResponseDto?> ValidateUserAsync(string email, string password)
+        public async Task<UserClaimsDto?> ValidateUserAsync(LoginRequestDto loginDto)
         {
-            var user = await _dbContext.Users.SingleOrDefaultAsync(u => u.Email == email);
-            if (user == null || user.Password != password)  // TODO: Hashish
+            var user = await _dbContext.Users.SingleOrDefaultAsync(u => u.Email == loginDto.Email);
+            if (user == null || user.Password != loginDto.Password)  // TODO: Hashish
             {
                 return null;
             }
 
-            return new AuthResponseDto
+            return new UserClaimsDto
             {
                 UserId = user.Id
             };
         }
 
-        public string GenJwtToken(int userId)
+        public string GenJwtToken(UserClaimsDto userClaims)
         {
-            throw new NotImplementedException();
+            string string_key = _config["Jwt:Key"] 
+                ?? throw new MissingConfigurationException("Jwt-Key not found in appsettings.json");
+            byte[] key = Encoding.ASCII.GetBytes(string_key);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userClaims.UserId.ToString())
+            };
+
+            string string_expiresIn = _config["Jwt:ExpiresInMinutes"]
+                ?? throw new MissingConfigurationException("Jwt-ExpiresInMinutes not found in appsettings.json");
+
+            if (!int.TryParse(string_expiresIn, out int expiresIn))
+                throw new FormatException("Jwt-ExpiresInMinutes does not have the correct int-format");
+
+            var descriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddMinutes(expiresIn),
+                Issuer = _config["Jwt:Issuer"]
+                    ?? throw new MissingConfigurationException("Jwt-Issuer not found in appsettings.json"),
+                Audience = _config["Jwt:Audience"]
+                    ?? throw new MissingConfigurationException("Jwt-Audience not found in appsettings.json"),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature
+                )
+            };
+
+            var handler = new JwtSecurityTokenHandler();
+            return handler.WriteToken(handler.CreateToken(descriptor));
         }
     }
 }
--- Api/Services/Auth/IAuthService.cs
+++ Api/Services/Auth/IAuthService.cs
@@ -1,12 +1,33 @@
 using Api.DTOs.Auth;
+using Api.Exceptions;
 
 
 namespace Api.Services.Auth
 {
     public interface IAuthService
     {
-        Task<AuthResponseDto> RegisterUserAsync(string email, string password);
-        Task<AuthResponseDto?> ValidateUserAsync(string email, string password);
-        string GenJwtToken(int userId);
+        /// <summary>
+        /// Adds a new user to the database, if a user with this email doesnt exist already
+        /// </summary>
+        /// <param name="registerDto">The register request (we assume the passwords match!)</param>
+        /// <returns>An object containing everything thats passed to the JWT</returns>
+        /// <exception cref="AlreadyExistsException">If the user already exists</exception>
+        Task<UserClaimsDto> RegisterUserAsync(RegisterRequestDto registerDto);
+        /// <summary>
+        /// Checks if a user with specified email exists, and if the password is corrent.
+        /// Then returns a corresponding UserClaimsDto that can be used to create a JWT (or null)
+        /// </summary>
+        /// <param name="loginDto">The password + email</param>
+        /// <returns>A ClaimsDto containing everything thats passed to the JWT OR null if the email / password is incorrect</returns>
+        Task<UserClaimsDto?> ValidateUserAsync(LoginRequestDto loginDto);
+        /// <summary>
+        /// Generates a JWT with the given claims, and the config specified in a appsettings.json.
+        /// </summary>
+        /// <param name="userClaims">The claims that are contained in the JWT.</param>
+        /// <returns>The JWT as string</returns>
+        /// <exception cref="MissingConfigurationException">If any of the following keys are not found in appsettings.json:
+        /// Jwt:Key (nur ASCII), Jwt:ExpiresInMinutes, Jwt:Issuer, Jwt:Audience</exception>
+        /// <exception cref="FormatException">If Jwt:ExpiresInMinutes does not have the correct int-format</exception>
+        string GenJwtToken(UserClaimsDto userClaims);
     }
 }
