47b8ea03b08dd57e77fe98ddb852fc486b0758b2
MoritzLue24 <moluecke@gmx.de>
2026-04-06 23:53:42 +0200

    Renamed AuthResponse to UserClaims

--- Api/DTOs/Auth/AuthResponseDto.cs
+++
@@ -1,7 +0,0 @@
-namespace Api.DTOs.Auth
-{
-    public class AuthResponseDto    // FIXME: Name vllt ändern? Missverständlich?
-    {
-        public int UserId { get; set; }
-    }
-}
---
+++ Api/DTOs/Auth/UserClaimsDto.cs
@@ -0,0 +1,10 @@
+namespace Api.DTOs.Auth
+{
+    /// <summary>
+    /// All fields are stored in a JWT
+    /// </summary>
+    public class UserClaimsDto
+    {
+        public int UserId { get; set; }
+    }
+}
