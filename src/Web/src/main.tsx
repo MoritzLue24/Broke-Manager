4ef068f783c342e2103b30f56d9ca5c34e140c1a
MoritzLue24 <moluecke@gmx.de>
2026-04-12 18:31:19 +0200

    fixed typo

--- src/Web/src/main.tsx
+++ src/Web/src/main.tsx
@@ -6,8 +6,8 @@ import "./index.css";
 import Login from "./pages/Login.tsx";
 import Register from "./pages/Register.tsx";
 import Dashboard from "./pages/Dashboard.tsx";
-import NotFound from "./pages/NotFound.tsx";
 import Categories from "./pages/Categories.tsx";
+import NotFound from "./pages/NotFound.tsx";
 
 
 const router = createBrowserRouter([
