5a31ecede6c42851a95df0a803e936ae09b40a38
burlitskiy <leonid.burlitckii@gmail.com>
2026-05-02 19:23:14 +0200

    Transcation Entity Contrsucotr added

--- src/Domain/Entities/Transaction.cs
+++ src/Domain/Entities/Transaction.cs
@@ -1,10 +1,11 @@
+
 using Domain.Common;
 using Domain.Enums;
 using Domain.ValueObjects;
 
 namespace Domain.Entities;
 
-public class Transcation
+public class Transaction
 {
     
         public Guid Id { get; private set;}
@@ -16,7 +17,47 @@ public class Transcation
         public string Title {get; private set;}
         public string CounterParty {get; private set;}
 
-        public RecurringDetail recurringDetail {get; private set;}
+        public RecurringDetail? RecurringDetail {get; private set;}
+
+
+        private Transaction(
+                Guid userId, 
+                Guid categoryId, 
+                CategorySource categorySource,
+                decimal amount, 
+                DateOnly date, 
+                string title, 
+                RecurringDetail? 
+                recurringDetail, 
+                string counterParty)
+        {
+                Id = Guid.NewGuid();
+                UserId = userId;
+                CategoryId = categoryId;
+                CategorySource = categorySource;
+                Amount = amount;
+                Date = date;
+                Title = title;
+                CounterParty = counterParty;
+                RecurringDetail = recurringDetail;
+                
+        }
+
+        public static DomainResult<Transaction> Create(
+                Guid userId, 
+                Guid categoryId, 
+                CategorySource categorySource,
+                decimal amount, 
+                DateOnly date, 
+                string title, 
+                RecurringDetail? 
+                recurringDetail, 
+                string counterParty)
+        {
+                
+        }
+
+
 
 
 
