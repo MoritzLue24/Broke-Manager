ad58c7342ab6b1256e6123c5e9f41214d6fcecf3
burlitskiy <leonid.burlitckii@gmail.com>
2026-05-11 00:01:29 +0200

    Test Keyword tests added

---
+++ tests/Domain.Tests/ValueObjects/KeywordTests.cs
@@ -0,0 +1,29 @@
+using Domain.Common;
+using Domain.ValueObjects;
+
+namespace Domain.Tests.ValueObjects;
+
+public class KeywordTests
+{
+    [Fact]
+    public void Create_ShouldReturnKeyword_WhenNotEmpty()
+    {
+        
+        var result= Keyword.Create("penis");
+
+        Assert.True(result.Success);
+        Assert.Equal("penis", result.Value.Value);
+        Assert.Throws<InvalidOperationException>(() => {var _ = result.Error;});
+    }
+
+    [Fact]
+    public void Create_ShouldReturnKeywordEmpty_WhenEmpty()
+    {
+        
+        var result = Keyword.Create("");
+
+        Assert.False(result.Success);
+        Assert.Equal(DomainErrorCode.KeywordEmpty, result.Error);
+        Assert.Throws<InvalidOperationException>(() => result.Value);
+    }
+}
