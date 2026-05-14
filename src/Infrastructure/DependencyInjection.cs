e77bf1833eb24ea2b71d560196c5ac5b2cb895e6
MoritzLue24 <moluecke@gmx.de>
2026-05-14 20:32:10 +0200

    added uow, repository services registration

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -1,6 +1,9 @@
 using Microsoft.Extensions.DependencyInjection;
 using Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
+using Application.Common.Interfaces;
+using Infrastructure.Persistence.Repositories;
+using Application.Features.Transactions;
 
 namespace Infrastructure;
 
@@ -10,6 +13,11 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, string connectionString)
     {
         services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
+
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<ICategoryReaderRepository, CategoryReaderRepository>();
+        services.AddScoped<ITransactionRepository, TransactionRepository>();
+
         return services;
     }
 }
