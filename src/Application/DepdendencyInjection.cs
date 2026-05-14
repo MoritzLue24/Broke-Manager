e88c72fa0e759aba38f724ba36d52b3256c366bf
MoritzLue24 <moluecke@gmx.de>
2026-05-14 20:32:29 +0200

    added mediatR DI

--- src/Application/DepdendencyInjection.cs
+++ src/Application/DepdendencyInjection.cs
@@ -6,6 +6,9 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
+        services.AddMediatR(
+            config => config.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly)
+        );
         return services;
     }
 }
