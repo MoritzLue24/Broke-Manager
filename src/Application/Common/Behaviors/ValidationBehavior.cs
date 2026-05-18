6fcc0800670ae30752b598e61846ceb7b251e4ed
MoritzLue24 <moluecke@gmx.de>
2026-05-19 00:27:36 +0200

    `ValidationBehavior`-setup integrated + Validator injection

---
+++ src/Application/Common/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,49 @@
+using Domain.Common;
+using FluentValidation;
+using MediatR;
+
+namespace Application.Common.Behaviors;
+
+public class ValidationBehavior<TRequest, TResponse>
+    : IPipelineBehavior<TRequest, TResponse>
+    // : IPipelineBehavior<TRequest, Result<TResponse>>
+    // where TRequest : IRequest<Result<TResponse>> DOES NOT WORK
+    // ^ in dotnet, DI with nested generics sometimes do not work.
+    // if we used this commented-out-approach, our behavior never gets called :/
+    where TRequest : notnull
+{
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+    public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+        => _validators = validators;
+
+    public async Task<TResponse> Handle(
+        TRequest request,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        if (!_validators.Any())
+            return await next(cancellationToken);
+
+        // Task.WhenAll hab ich nicht wirklich gecheckt :/
+        var validationResults = await Task.WhenAll(_validators.Select(async v
+            => await v.ValidateAsync(request, cancellationToken)
+        ));
+
+        if (validationResults.All(v => v.IsValid))
+            return await next(cancellationToken);
+
+        var errors = validationResults
+            .SelectMany(v => v.Errors)  // Flatten nested collection
+            .Select(error => new ValidationError(
+                error.PropertyName,
+                error.ErrorMessage
+            ));
+
+        // We have to cast this during runtime (ugly),
+        // but theres no other option since we have this very unclear 
+        // `TResponse` generic as return type
+        // FIXME: Find other, more cleaner, approaches
+        return (dynamic)errors.Cast<Error>().ToList();
+    }
+}
--- src/Application/DepdendencyInjection.cs
+++ src/Application/DepdendencyInjection.cs
@@ -1,3 +1,9 @@
+using Application.Common.Behaviors;
+using Application.Features.Transactions;
+using Application.Features.Transactions.Commands.CreateTransaction;
+using Domain.Common;
+using FluentValidation;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Application;
@@ -6,9 +12,12 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        services.AddMediatR(
-            config => config.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly)
-        );
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+            config.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+        });
+        services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
         return services;
     }
 }
