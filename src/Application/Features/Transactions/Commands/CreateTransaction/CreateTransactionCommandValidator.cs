a43edf5785685b77e4d4c36d96fbe3c2f8724f14
MoritzLue24 <moluecke@gmx.de>
2026-05-19 23:14:52 +0200

    CreateTransactionCommandValidator started

---
+++ src/Application/Features/Transactions/Commands/CreateTransaction/CreateTransactionCommandValidator.cs
@@ -0,0 +1,12 @@
+using FluentValidation;
+
+namespace Application.Features.Transactions.Commands.CreateTransaction;
+
+public class CreateTransactionCommandValidator : AbstractValidator<CreateTransactionCommand>
+{
+    public CreateTransactionCommandValidator()
+    {
+        this.RuleFor(x => x.Amount)
+            .GreaterThan(0);
+    }
+}
