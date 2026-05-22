2024ee81b46ba20a3c5ecba7c7a54e5bae696728
MoritzLue24 <moluecke@gmx.de>
2026-05-22 20:12:06 +0200

    Comment

--- src/Api/Configuration/JwtBearerOptionsSetup.cs
+++ src/Api/Configuration/JwtBearerOptionsSetup.cs
@@ -5,6 +5,8 @@ using Microsoft.Extensions.Options;
 
 namespace Api.Configuration;
 
+// Configures the http side of JwtBearerOptions, because
+// we dont want to change http things like responses inside the Infrastructure layer
 public class JwtBearerOptionsSetup : IConfigureNamedOptions<JwtBearerOptions>
 {
     public void Configure(string? name, JwtBearerOptions options)
