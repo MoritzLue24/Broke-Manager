7874410c3e1c440ef497705a800be5c293efa80d
MoritzLue24 <moluecke@gmx.de>
2026-05-24 21:57:54 +0200

    Initial Api tests commit

---
+++ tests/Api.Tests/TestInfrastructure/Controllers/WebAppCollection.cs
@@ -0,0 +1,8 @@
+namespace Api.Tests.TestInfrastructure.Controllers;
+
+// Defines a collection so that only one instance of
+// Postgres fixture gets created.
+// All persistence test classes should use this collection
+// Gets injected because we implemented `ICollectionFixture`
+[CollectionDefinition("WebAppCollection")]
+public class WebAppCollection : ICollectionFixture<WebAppFactory> {}
---
+++ tests/Api.Tests/TestInfrastructure/Controllers/WebAppFactory.cs
@@ -0,0 +1,41 @@
+using Infrastructure.Persistence;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Testcontainers.PostgreSql;
+
+namespace Api.Tests.TestInfrastructure.Controllers;
+
+public class WebAppFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    private readonly PostgreSqlContainer _postgresContainer
+         = new PostgreSqlBuilder("postgres:latest")
+            .WithDatabase("broke-manager-tests")
+            .WithUsername("root-tests")
+            .WithPassword("root-tests123!")
+            .WithCleanUp(true)
+            .WithAutoRemove(true)
+            .Build();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseSetting("ConnectionStrings:DefaultConnection", this._postgresContainer.GetConnectionString());
+        builder.UseSetting("JwtSettings:Secret", "test-secret-key-min-32-characters!!");
+    }
+
+    public async Task InitializeAsync()
+    {
+        await this._postgresContainer.StartAsync();
+
+        var db = new AppDbContext(new DbContextOptionsBuilder<AppDbContext>()
+            .UseNpgsql(this._postgresContainer.GetConnectionString())
+            .Options);
+        await db.Database.MigrateAsync();
+        await db.DisposeAsync();
+    }
+
+    async Task IAsyncLifetime.DisposeAsync()
+    {
+        await this._postgresContainer.DisposeAsync();
+    }
+}
