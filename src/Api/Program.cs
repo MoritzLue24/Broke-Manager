f6470d5d42c712b7ffd7d11be7e402b36775352b
MoritzLue24 <moluecke@gmx.de>
2026-05-24 21:50:36 +0200

    created program class as a marker, needed for testing

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -3,31 +3,38 @@ using Api.Middlewares;
 using Application;
 using Infrastructure;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace Api;
+
+public class Program
 {
-    builder.Configuration.AddEnvironmentVariables();
-
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
-
-    builder.Services.AddInfrastructure(builder.Configuration);
-    builder.Services.AddApplication();
-    builder.Services.AddApi();
-}
-
-var app = builder.Build();
-{
-    if (app.Environment.IsDevelopment())
+    public static void Main(string[] args)
     {
-        app.UseSwagger();
-        app.UseSwaggerUI();
+        var builder = WebApplication.CreateBuilder(args);
+        {
+            builder.Configuration.AddEnvironmentVariables();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddInfrastructure(builder.Configuration);
+            builder.Services.AddApplication();
+            builder.Services.AddApi();
+        }
+
+        var app = builder.Build();
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            // app.UseHttpsRedirection();
+            app.UseMiddleware<ExceptionMiddleware>();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.Run();
+        }
     }
-
-    // app.UseHttpsRedirection();
-    app.UseMiddleware<ExceptionMiddleware>();
-    app.UseAuthentication();
-    app.UseAuthorization();
-    app.MapControllers();
-    app.Run();
 }
-
