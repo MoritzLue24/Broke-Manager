3b3a0bc2299faacd60982c5f8af25051f7ce9d06
MoritzLue24 <moluecke@gmx.de>
2026-06-01 00:55:41 +0200

    Created default category on UserCreatedEvent

--- src/Application/Features/Auth/Events/UserCreated/UserCreatedNotificationHandler.cs
+++ src/Application/Features/Auth/Events/UserCreated/UserCreatedNotificationHandler.cs
@@ -1,11 +1,31 @@
+using Application.Common.Interfaces.Persistence;
+using Domain.Entities;
 using MediatR;
 
 namespace Application.Features.Auth.Events.UserCreated;
 
 public class UserCreatedHandler : INotificationHandler<UserCreatedNotification>
 {
-    public Task Handle(UserCreatedNotification notification, CancellationToken cancellationToken)
+#pragma warning disable IDE0052 // Remove unread private members
+    private readonly IUnitOfWork _uow;
+    private readonly ICategoryRepository _categoryRepo;
+#pragma warning restore IDE0052 // Remove unread private members
+
+    public UserCreatedHandler(IUnitOfWork uow, ICategoryRepository categoryRepo)
     {
-        return Task.CompletedTask;
+        this._uow = uow;
+        this._categoryRepo = categoryRepo;
+    }
+
+    public async Task Handle(UserCreatedNotification notification, CancellationToken cancellationToken)
+    {
+        var defaultCategory = Category.Create(
+            notification.UserId,
+            "Default",   // TODO: Custom name??
+            true
+        ).Value;    // We assume correct parameters, because all parameters are created & verified by the program
+
+        this._categoryRepo.Add(defaultCategory);
+        await this._uow.SaveChangesAsync(cancellationToken);
     }
 }
