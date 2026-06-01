ceb29be5beb05aa4c5476bb5bc6adaaffe8a3058
MoritzLue24 <moluecke@gmx.de>
2026-06-01 23:02:58 +0200

    IRequire Authorization implemented

--- src/Application/Features/Transactions/Queries/GetTransaction/GetTransactionQuery.cs
+++ src/Application/Features/Transactions/Queries/GetTransaction/GetTransactionQuery.cs
@@ -1,9 +1,16 @@
+using Application.Common.Interfaces.Security;
 using Application.Features.Transactions.Common;
 using Domain.Common;
+using Domain.Enums;
 using MediatR;
 
 namespace Application.Features.Transactions.Queries.GetTransaction;
 
 public record GetTransactionQuery(
     Guid TransactionId
-) : IRequest<Result<TransactionResult>>;
+) : IRequest<Result<TransactionResult>>, IRequireAuthorization
+{
+    // Admins and Users can execute this command
+    // (not both roles are needed)
+    public Role[] Roles => [Role.User, Role.Admin];
+}
--- src/Application/Features/Transactions/Queries/GetTransactionsByUser/GetTransactionsByUserQuery.cs
+++ src/Application/Features/Transactions/Queries/GetTransactionsByUser/GetTransactionsByUserQuery.cs
@@ -1,8 +1,15 @@
+using Application.Common.Interfaces.Security;
 using Application.Features.Transactions.Common;
 using Domain.Common;
+using Domain.Enums;
 using MediatR;
 
 namespace Application.Features.Transactions.Queries.GetTransactionsByUser;
 
 public record GetTransactionsByUserQuery(   // TODO: with pages etc.
-) : IRequest<Result<List<TransactionResult>>>;
+) : IRequest<Result<List<TransactionResult>>>, IRequireAuthorization
+{
+    // Admins and Users can execute this command
+    // (not both roles are needed)
+    public Role[] Roles => [Role.User, Role.Admin];
+}
