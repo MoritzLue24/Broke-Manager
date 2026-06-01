4636fa5324281d5c3b114abeeb5fd437786441e7
MoritzLue24 <moluecke@gmx.de>
2026-06-01 22:28:50 +0200

    Deleted several testing projects because it was too much work
    - Api unit tests
    - Infrastructure tests
    - App tests

--- tests/Infrastructure.IntegrationTests/TestInfrastructure/Persistence/PostgresCollection.cs
+++
@@ -1,8 +0,0 @@
-namespace Infrastructure.IntegrationTests.TestInfrastructure.Persistence;
-
-// Defines a collection so that only one instance of
-// Postgres fixture gets created.
-// All persistence test classes should use this collection
-// Gets injected because we implemented `ICollectionFixture`
-[CollectionDefinition("PostgresCollection")]
-public class PostgresCollection : ICollectionFixture<PostgresFixture> { }
