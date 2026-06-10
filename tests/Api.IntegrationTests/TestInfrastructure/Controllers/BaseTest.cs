41a0943b4e60b8e17bd5072fd66fd985a419543e
MoritzLue24 <moluecke@gmx.de>
2026-06-10 17:10:01 +0200

    CreateMockUser

--- tests/Api.IntegrationTests/TestInfrastructure/Controllers/BaseTest.cs
+++ tests/Api.IntegrationTests/TestInfrastructure/Controllers/BaseTest.cs
@@ -1,5 +1,8 @@
 using Application.Common.Interfaces.Security;
+using Domain.Entities;
+using Domain.ValueObjects;
 using Infrastructure.Persistence;
+using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -9,6 +12,7 @@ namespace Api.IntegrationTests.TestInfrastructure.Controllers;
 public abstract class BaseTest : IAsyncLifetime
 {
     private readonly IServiceScope _scope;
+    private readonly IHasher _hasher;
     protected HttpClient Client { get; private init; }
     protected AppDbContext Db { get; private init; }
     protected IJwtTokenGenerator JwtTokenGenerator { get; private init; }
@@ -18,9 +22,20 @@ public abstract class BaseTest : IAsyncLifetime
         // We need to create a scope manually, because the database is created as a scoped service
         // Normally, aspdotnet handles that for us
         this._scope = factory.Services.CreateScope();
-        this.Client = factory.CreateClient();
+        this._hasher = factory.Services.GetRequiredService<IHasher>();
+
+        this.Client = factory.CreateClient(new WebApplicationFactoryClientOptions { HandleCookies = true });
         this.Db = this._scope.ServiceProvider.GetRequiredService<AppDbContext>();   // because scoped
         this.JwtTokenGenerator = factory.Services.GetRequiredService<IJwtTokenGenerator>(); // because singleton
+    }
+
+    protected void CreateMockUser(string email, string password)
+    {
+        this.Db.Users.Add(User.Create(
+            Email.Create(email).Value,
+            Hash.Create(this._hasher.Hash(password)).Value
+        ).Value);
+        this.Db.SaveChangesAsync();
     }
 
     public Task InitializeAsync()
