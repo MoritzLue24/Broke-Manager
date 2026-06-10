db9609b3ba8eaa442c46586822833a4337154a11
MoritzLue24 <moluecke@gmx.de>
2026-06-11 00:01:55 +0200

    removed unnessecascary base class

--- src/Application/Common/Exceptions.cs
+++ src/Application/Common/Exceptions.cs
@@ -1,11 +1,5 @@
 namespace Application.Common;
 
-public abstract class BaseException(string message) : Exception(message)
-{
-    public override string ToString()
-        => this.GetType().ToString();
-}
-
 // Thrown on unrecoverable errors on invalid states that violates certain invariants
 // for example on NotificationHandlers, we cant return a Result type -> unrecoverable
-public class InvariantViolationException(string message) : BaseException(message);
+public class InvariantViolationException(string message) : Exception(message);
