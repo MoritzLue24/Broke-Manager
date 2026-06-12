a4ce50f77d20d307033b1d8746bbe4b514072d57
MoritzLue24 <moluecke@gmx.de>
2026-06-12 18:20:24 +0200

    Formatting

--- src/Api/Errors/ErrorTypes.cs
+++ src/Api/Errors/ErrorTypes.cs
@@ -7,21 +7,21 @@ public static class ErrorTypes
     private const string Base = "urn:error";
 
     // Common
-    public const string Internal                = $"{Base}:internal";
-    public const string Validation              = $"{Base}:validation";
-    public const string NotFound                = $"{Base}:not-found";
-    public const string Duplicate               = $"{Base}:duplicate";
+    public const string Internal = $"{Base}:internal";
+    public const string Validation = $"{Base}:validation";
+    public const string NotFound = $"{Base}:not-found";
+    public const string Duplicate = $"{Base}:duplicate";
 
     // Auth
-    public const string Unauthorized            = $"{Base}:auth:unauthorized";
-    public const string Forbidden               = $"{Base}:auth:forbidden";
-    public const string InvalidCredentials      = $"{Base}:auth:invalid-credentials";
-    public const string TokenInvalid            = $"{Base}:auth:token-invalid";
+    public const string Unauthorized = $"{Base}:auth:unauthorized";
+    public const string Forbidden = $"{Base}:auth:forbidden";
+    public const string InvalidCredentials = $"{Base}:auth:invalid-credentials";
+    public const string TokenInvalid = $"{Base}:auth:token-invalid";
 
     // Users
     public const string PropertyAlreadyAssigned = $"{Base}:user:property-already-assigned";
 
     // Categories
-    public const string CategoryIsDefault       = $"{Base}:category:is-default";
+    public const string CategoryIsDefault = $"{Base}:category:is-default";
     public const string DefaultCategoryNotFound = $"{Base}:category:default-not-found";
 }
